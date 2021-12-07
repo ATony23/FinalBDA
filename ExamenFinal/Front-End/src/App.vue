bb7484bc38ce1e6183a4f922630f4a71a4f78c63
ATony23 <83042213+ATony23@users.noreply.github.com>
2021-12-06 19:12:56 -0700

    aa
    aa

---
+++ ExamenFinal/Front-End/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  background-color: #073ea3;
+  height: 100vh;
+}
+</style>
